8855388ddfe6945e677252c07227618c1f0b31e9
abhishek-nirala <doctordeath918@gmail.com>
2024-09-25 21:25:32 +0530

    card component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import FootBar from "./Components/Footer/FootBar.jsx";
 import Header from "./Components/Header/HeadBar.jsx";
+import Home from "./Page/Home/Home.jsx";
 
 function App() {
   return (
     <>
       <Header />
-      <FootBar/>
+      <Home/>
+      <FootBar />
     </>
   );
 }
---
+++ src/Page/Home/Home.jsx
@@ -0,0 +1,13 @@
+import "./Home.css"
+import Card from '../../Components/Card/Card.jsx'
+function Home(){
+
+    return<main>
+        <div className="main">
+            <div className="container">
+            <Card/>
+            </div>
+        </div>
+    </main>
+}
+export default Home
