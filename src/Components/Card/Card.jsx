c60d47b106aee4b5e2eafac2338b36a24d64811a
abhishek-nirala <doctordeath918@gmail.com>
2024-09-25 21:26:28 +0530

    card component added to Page/home

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import "./card.css";
+
+function Card() {
+  return (
+    <div className="">
+      <div className="imgDiv">
+        <img src="" />
+      </div>
+    </div>
+  );
+}
+export default Card;
