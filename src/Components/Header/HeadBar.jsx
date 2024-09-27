618511cfeb077372db87487c8b028fefa2e813a9
abhishek-nirala <doctordeath918@gmail.com>
2024-09-27 17:32:17 +0530

    some minor changes and responsivness added

--- src/Components/Header/HeadBar.jsx
+++ src/Components/Header/HeadBar.jsx
@@ -1,19 +1,13 @@
-
-import './header.css'
+import "./header.css";
 
 function Header() {
-    return <>
-    <nav className='nav-bar'> 
-    <div className="head">
-      <ul >
-          <li className='li'>Home</li>
-          <li className='li'>Books</li>
-          <li className='li'>Authors</li>
-      </ul>
-    </div> 
-    </nav>
-    </>;
-  }
-  
-  export default Header
-  
+  return (
+    <>
+      <nav className="nav-bar">
+        <h2>BookKeeper</h2>
+      </nav>
+    </>
+  );
+}
+
+export default Header;
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,22 +1,24 @@
-import "./Home.css"
-import Card from '../../Components/Card/Card.jsx'
-import items from '../../assets/items.json'
-function Home(){
-
-    return<main>
-        <div className="main">
-            <div className="container">
-            <Card link={items.items[0].Link} tittle={items.items[0].Title} author={items.items[0].author}/>
-            <Card className="card1"link={items.items[1].Link} tittle={items.items[1].Title} author={items.items[1].author}/>
-            <Card link={items.items[2].Link} tittle={items.items[2].Title} author={items.items[2].author}/>
-            <Card link={items.items[0].Link} tittle={items.items[0].Title} author={items.items[0].author}/>
-            <Card className="card1"link={items.items[1].Link} tittle={items.items[1].Title} author={items.items[1].author}/>
-            <Card link={items.items[2].Link} tittle={items.items[2].Title} author={items.items[2].author}/>
-            <Card link={items.items[0].Link} tittle={items.items[0].Title} author={items.items[0].author}/>
-            <Card className="card1"link={items.items[1].Link} tittle={items.items[1].Title} author={items.items[1].author}/>
-            <Card link={items.items[2].Link} tittle={items.items[2].Title} author={items.items[2].author}/>
-            </div>
+import "./Home.css";
+import Card from "../../Components/Card/Card.jsx";
+import items from "../../assets/items.json";
+function Home() {
+  return (
+    <main>
+      <div className="main">
+        <div className="container">
+          {items.items.map((item) => {
+            return (
+              <Card
+                key="index"
+                link={item.Link}
+                tittle={item.Title}
+                author={item.author}
+              />
+            );
+          })}
         </div>
+      </div>
     </main>
+  );
 }
-export default Home
+export default Home;
